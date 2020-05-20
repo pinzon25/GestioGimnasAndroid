451a5635b70e5dac26d85b57724b3d0275fdb6ea
Ricard <ricardpinzonsuller@gmail.com>
2020-05-20 11:49:26 +0200

    -Modificada la classe EncriptaDesencripta.

--- app/src/main/java/ricard/projecte/gestiogimnasandroid/EncriptaDesencripta.java
+++ app/src/main/java/ricard/projecte/gestiogimnasandroid/EncriptaDesencripta.java
@@ -1,16 +1,23 @@
 package ricard.projecte.gestiogimnasandroid;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import java.io.UnsupportedEncodingException;
+import java.util.Base64;
 
 public class EncriptaDesencripta {
 
-    /*public static String Encriptar(String texto) throws UnsupportedEncodingException {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static String Encriptar(String texto) throws UnsupportedEncodingException {
 
-        return Base64.encodeBase64String(texto.getBytes("utf-8")); //devuelve un string codificado.
+        return Base64.getEncoder().encodeToString(texto.getBytes("utf-8")); //devuelve un string codificado.
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public static String Desencriptar(String textoEncriptado) throws Exception {
-        byte[] decode = Base64.decodeBase64(textoEncriptado.getBytes()); //decodifica el string codificado y lo devuelve.
+        byte[] decode = Base64.getDecoder().decode(textoEncriptado.getBytes()); //decodifica el string codificado y lo devuelve.
         return new String(decode, "utf-8");
-    }*/
+    }
 }
